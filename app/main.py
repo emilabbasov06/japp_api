11fcee7c52311930023bc0c90b750eb4d67ec465
emilabbasov06 <emilabb13@gmail.com>
2025-02-04 16:42:48 +0400

    worked on authentication

---
+++ app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from .routers import category, company, vacancy, auth
+
+app = FastAPI()
+
+origins = [
+  'http://localhost:5173',
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=['*'],
+  allow_credentials=True,
+  allow_methods=['*'],
+  allow_headers=['*'],
+)
+
+app.include_router(auth.router)
+app.include_router(category.router)
+app.include_router(company.router)
+app.include_router(vacancy.router)
+
+@app.get('/')
+def index():
+  return {'message': 'Welcome to a JobApp API'}
